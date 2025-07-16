f3818f351b45233d56f52263f611f2747ab6ee7c
Sam Stevens <6634981+the-inconvenience-store@users.noreply.github.com>
2025-07-16 20:58:12 +1000

    chore: various changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,14 +4,13 @@ export default async function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        My Blog
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`}
       </p>
       <div className="my-8">
         <BlogPosts />
